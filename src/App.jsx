99799082796875b3383724c6ab35d98d76b29528
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-02 22:20:40 -0700

    added context to set menu active and opposite

--- src/App.jsx
+++ src/App.jsx
@@ -24,10 +24,12 @@ import {
   Editor,
 } from "./pages";
 
+import { useStateContext } from "./context/ContextProvider";
+
 import "./App.css";
 
 const App = () => {
-  const activeMenu = true;
+  const { activeMenu } = useStateContext();
   return (
     <div>
       <BrowserRouter>
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import { Link, NavLink } from "react-router-dom";
 import { SiShopware } from "react-icons/si";
@@ -7,8 +7,10 @@ import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 
 import { links } from "../data/dummy";
 
+import { useStateContext } from "../context/ContextProvider";
+
 const Sidebar = () => {
-  const activeMenu = true;
+  const { activeMenu, setActiveMenu } = useStateContext();
 
   const activeLink =
     "flex items-center gap-5 pl-4 pt-3 pb-2.5 rounded-lg text-white text-md m-2";
@@ -22,16 +24,16 @@ const Sidebar = () => {
           <div className="flex justify-between items-center">
             <Link
               to="/"
-              onClick={() => {}}
+              onClick={() => setActiveMenu(false)}
               className="items-center gap-3 ml-3 mt-4 flex text-xl font-extrabold tracking-tight dark:text-white text-slate-900"
             >
               <SiShopware /> <span>Shoppy</span>
             </Link>
             <TooltipComponent content="Menu" position="BottomCenter">
               <button
-                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block md:hidden"
+                className="text-xl rounded-full p-3 hover:bg-light-gray mt-4 block"
                 type="button"
-                onClick={() => {}}
+                onClick={() => setActiveMenu((prevActiveMenu) => !prevActiveMenu)}
               >
                 <MdOutlineCancel />
               </button>
