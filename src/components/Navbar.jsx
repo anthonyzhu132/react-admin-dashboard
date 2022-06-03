050c6beaa09c6e1c75ed5f19dbbae6f00082f279
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-02 22:47:01 -0700

    added style and boilerplate for Navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,8 +11,86 @@ import { Cart, Chat, Notification, UserProfile } from ".";
 
 import { useStateContext } from "../context/ContextProvider";
 
+const NavButton = ({ title, customFunction, icon, color, dotColor }) => {
+  return (
+    <TooltipComponent content={title} position="BottomCenter">
+      <button
+        type="button"
+        onClick={() => customFunction()}
+        style={{ color }}
+        className="relative text-xl rounded-full p-3 hover:bg-light-gray"
+      >
+        <span
+          style={{ background: dotColor }}
+          className="absolute inline-flex rounded-full h-2 w-2 right-2 top-2"
+        />
+        {icon}
+      </button>
+    </TooltipComponent>
+  );
+};
+
+const handleClick = () => {};
+
 const Navbar = () => {
-  return <div>Navbar</div>;
+  const { activeMenu, setActiveMenu } = useStateContext();
+
+  return (
+    <div className="flex justify-between p-2 md:mx-6 relative">
+      <NavButton
+        title="Menu"
+        customFunction={() =>
+          setActiveMenu((prevActiveMenu) => !prevActiveMenu)
+        }
+        color="blue"
+        icon={<AiOutlineMenu />}
+      />
+
+      <div className="flex">
+        <NavButton
+          title="Cart"
+          customFunction={() => handleClick("cart")}
+          color="blue"
+          icon={<FiShoppingCart />}
+        />
+
+        <NavButton
+          title="Chat"
+          dotColor="#03C9D7"
+          customFunction={() => handleClick("chat")}
+          color="blue"
+          icon={<BsChatLeft />}
+        />
+
+        <NavButton
+          title="Notification"
+          customFunction={() => handleClick("notification")}
+          color="blue"
+          icon={<RiNotification3Line />}
+        />
+
+        <TooltipComponent content="Profile" position="BottomCenter">
+          <div
+            className="flex item-center gap-2 cursor-pointer p-1 hover:bg-light-gray rounded-lg"
+            onClick={() => handleClick("userProfile")}
+          >
+            <img
+              src={avatar}
+              alt="profileImage"
+              className="rounded-full w-8 h-8 "
+            />
+            <p>
+              <span className="text-gray-400 text-14">Hi, </span>{" "}
+              <span className="text-gray-400 font-bold ml-1 text-14">
+                Anthony
+              </span>
+            </p>
+            <MdKeyboardArrowDown className="text-gray-400 text-14" />
+          </div>
+        </TooltipComponent>
+      </div>
+    </div>
+  );
 };
 
 export default Navbar;
