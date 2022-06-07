19ca9951522416e9d78bdfd43f548a5e67a709d1
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-06 22:09:28 -0700

    adjusted StackedChart to map through data and to show as a bar chart

--- src/components/Charts/StackedChart.jsx
+++ src/components/Charts/StackedChart.jsx
@@ -1,9 +1,44 @@
-import React from 'react'
+import React from "react";
+import {
+  ChartComponent,
+  SeriesCollectionDirected,
+  SeriesDirective,
+  Inject,
+  Legend,
+  Category,
+  StackingColumnSeries,
+  Tooltip,
+  SeriesCollectionDirective,
+} from "@syncfusion/ej2-react-charts";
 
-const StackedChart = () => {
+import {
+  stackedCustomSeries,
+  stackedPrimaryXAxis,
+  stackedPrimaryYAxis,
+} from "../../data/dummy";
+
+const StackedChart = ({ width, height }) => {
   return (
-    <div>StackedChart</div>
-  )
-}
+    <ChartComponent
+      id="charts"
+      primaryXAxis={stackedPrimaryXAxis}
+      primaryYAxis={stackedPrimaryYAxis}
+      width={width}
+      height={height}
+      chartArea={{ border: { width: 0 } }}
+      tooltip={{ enable: true }}
+      // background={currentMode === "Dark" ? "#33373E" : "#fff"}
+      legendSettings={{ background: "white" }}
+    >
+      <Inject services={[StackingColumnSeries, Category, Legend, Tooltip]} />
+      <SeriesCollectionDirective>
+        {/* eslint-disable-next-line react/jsx-props-no-spreading */}
+        {stackedCustomSeries.map((item, index) => (
+          <SeriesDirective key={index} {...item} />
+        ))}
+      </SeriesCollectionDirective>
+    </ChartComponent>
+  );
+};
 
-export default StackedChart
+export default StackedChart;
