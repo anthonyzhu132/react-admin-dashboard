dfda999ea92734f637589d54a40e8b970aa9331e
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-06 22:17:40 -0700

    imported grid data

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -20,8 +20,15 @@ import { Header } from "../components";
 
 const Orders = () => {
   return (
-    <div>
+    <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header title="Orders" category="Page" />
+      <GridComponent id="gridcomp" dataSource={ordersData}>
+        <ColumnsDirective>
+          {ordersGrid.map((item, index) => {
+            return <ColumnDirective key={index} {...item} />;
+          })}
+        </ColumnsDirective>
+      </GridComponent>
     </div>
   );
 };
