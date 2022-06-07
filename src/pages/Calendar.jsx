542ec9ae936128aecaf45db83ba8d4dc1ccd4c79
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-06 22:49:02 -0700

    added calendar

--- src/pages/Calendar.jsx
+++ src/pages/Calendar.jsx
@@ -1,9 +1,40 @@
-import React from 'react'
+import React from "react";
+import {
+  Schedule,
+  ViewsDirective,
+  ViewDirective,
+  Day,
+  Week,
+  WorkWeek,
+  Month,
+  Agenda,
+  Inject,
+  Resize,
+  DragAndDrop,
+  ScheduleComponent,
+} from "@syncfusion/ej2-react-schedule";
+
+import { DatePicker } from "@syncfusion/ej2-react-calendars";
+
+import { scheduleData } from "../data/dummy";
+import { Header } from "../components";
 
 const Calendar = () => {
   return (
-    <div>Calendar</div>
-  )
-}
+    <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
+      <Header title="Calendar" category="App" />
 
-export default Calendar
+      <ScheduleComponent
+        height="650px"
+        eventSettings={{ dataSource: scheduleData }}
+        selectedDate={new Date(2021, 0, 10)}
+      >
+        <Inject
+          services={[Day, Week, WorkWeek, Month, Agenda, Resize, DragAndDrop]}
+        />
+      </ScheduleComponent>
+    </div>
+  );
+};
+
+export default Calendar;
