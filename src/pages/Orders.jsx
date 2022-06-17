b7aae764613ccbc721f4b457b92373b9f8de19f8
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-16 22:16:43 -0700

    added kanban with dummy data

--- src/pages/Orders.jsx
+++ src/pages/Orders.jsx
@@ -22,7 +22,13 @@ const Orders = () => {
   return (
     <div className="m-2 md:m-10 p-2 md:p-10 bg-white rounded-3xl">
       <Header title="Orders" category="Page" />
-      <GridComponent id="gridcomp" dataSource={ordersData} allowPaging allowSorting allowExcelExport>
+      <GridComponent
+        id="gridcomp"
+        dataSource={ordersData}
+        allowPaging
+        allowSorting
+        allowExcelExport
+      >
         <ColumnsDirective>
           {ordersGrid.map((item, index) => {
             return <ColumnDirective key={index} {...item} />;
