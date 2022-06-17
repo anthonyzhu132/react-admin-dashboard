2fa0bc53183184be22981802655c59f3c6cc7dfa
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-16 22:28:35 -0700

    added ability to change preview

--- src/pages/ColorPicker.jsx
+++ src/pages/ColorPicker.jsx
@@ -2,6 +2,12 @@ import React from "react";
 import { ColorPickerComponent } from "@syncfusion/ej2-react-inputs";
 import { Header } from "../components";
 
+//Function grabs the hex value of the changed hex from the Picker and Pallete
+const changeColor = (args) => {
+  document.getElementById("preview").style.backgroundColor =
+    args.currentValue.hex;
+};
+
 const ColorPicker = () => {
   return (
     <div className="m-2 md:m-10 mt-24 p-2 md:p-10 bg-white rounded-3xl">
@@ -18,6 +24,7 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={changeColor}
             />
           </div>
           <div>
@@ -28,6 +35,7 @@ const ColorPicker = () => {
               modeSwitcher={false}
               inline
               showButtons={false}
+              change={changeColor}
             />
           </div>
         </div>
