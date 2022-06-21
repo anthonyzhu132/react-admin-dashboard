9ecf952b40ccbf6dcf228bc2a81fffac562a6612
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-20 21:06:13 -0700

    added ability to select differnet themes, but no functionakity yet

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,5 +1,8 @@
+import { TooltipComponent } from "@syncfusion/ej2-react-popups";
 import React from "react";
+import { BsCheck } from "react-icons/bs";
 import { MdOutlineCancel } from "react-icons/md";
+import { themeColors } from "../data/dummy";
 
 const ThemeSettings = () => {
   return (
@@ -18,6 +21,60 @@ const ThemeSettings = () => {
         </div>
         <div className="flex-col border-t-1 border-color p-4 ml-4">
           <p className="font-semibold text-lg">Theme Options</p>
+
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="light"
+              name="theme"
+              value="Light"
+              className="cursor-pounter"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label htmlFor="light" className="ml-2 text-md cursor-pointer">
+              Light
+            </label>
+          </div>
+          <div className="mt-4">
+            <input
+              type="radio"
+              id="dark"
+              name="theme"
+              value="Dark"
+              className="cursor-pounter"
+              onChange={() => {}}
+              checked={true}
+            />
+            <label htmlFor="dark" className="ml-2 text-md cursor-pointer">
+              Dark
+            </label>
+          </div>
+        </div>
+
+        <div className="flex-col border-t-1 border-color p-4 ml-4">
+          <p className="font-semibold text-lg">Theme Colors</p>
+          <div className="flex gap-3">
+            {themeColors.map((index, item) => {
+              return (
+                <TooltipComponent
+                  key={index}
+                  content={item.name}
+                  position="TopCenter"
+                >
+                  <div className="relative mt-2 cursor-pointer flex gap-5 items-center">
+                    <button>
+                      <BsCheck
+                        className={`ml-2 text-2xl text-white ${
+                          item.color === currentColor ? "block" : "hidden"
+                        }`}
+                      />
+                    </button>
+                  </div>
+                </TooltipComponent>
+              );
+            })}
+          </div>
         </div>
       </div>
     </div>
