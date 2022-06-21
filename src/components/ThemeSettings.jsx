0abbadc1545ef1177847364fdb112a278adb847e
Anthony Zhu <anthonyzhu132@gmail.com>
2022-06-20 20:56:22 -0700

    added Theme settings

--- src/components/ThemeSettings.jsx
+++ src/components/ThemeSettings.jsx
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from "react";
+import { MdOutlineCancel } from "react-icons/md";
 
 const ThemeSettings = () => {
   return (
-    <div>ThemeSettings</div>
-  )
-}
+    <div className="bg-half-transparent w-screen fixed nav-item top-0 right-0">
+      <div className="float-right h-screen dark:text-gray-200 bg-white dark:[#484B52] w-400">
+        <div className="flex justify-between items-center p-4 ml-4">
+          <p className="font-semibold text-lg">Settings</p>
+          <button
+            type="button"
+            onClick={() => {}}
+            stlye={{ color: "rgb(153, 171, 180)", borderRadius: "50%" }}
+          >
+            <MdOutlineCancel />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
 
-export default ThemeSettings
+export default ThemeSettings;
